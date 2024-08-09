ca3c50732d119b73d028c1d6fe180bfe89592b63
Jota Santos <jailsonsantosdpge@outlook.com>
2024-08-09 14:38:14 -0400

    2.8. Incorporando expressões dentro do JSX

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,7 +1,14 @@
 export default function HelloWorld () {
+
+    const name = 'Jota Santos'
+
     return (
         <>
-            <h2>Hello World React.js !!!</h2>
+            <h2>
+                Hello World React.js, { name === 'Jota Santos' 
+                    ? name : 
+                    'Jessica' } !!!
+            </h2>
         </>
     )
 }
