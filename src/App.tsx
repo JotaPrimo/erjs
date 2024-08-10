05575bf68837d4591fcbadabb9d20274ad2896f4
jota santos <gestald118@email.com>
2024-08-10 09:09:25 -0400

    2.15. Como funcionam as props dentro do React

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import Clock from './Clock';
-import Person from './Person';
+import Post from './components/Post';
+import Sidebar from './components/Sidebar';
 
 
-// isto é um componente : uma função que retorna html
+// trabalhando com props
+// enviando props com expressoes js
 function App() {
+
+  // criar um state post
+  // toda vez que uma prop tem seu state alterado, o componente re-redenderiza
+  // por isso que quando se usar o set parta alterar o state post,  que é uma prop de post
+  // ele re-renderiza
+  const [post, setPost] = useState({
+    title: 'Título muito maneiro',
+    content: 'Lorem ipsum dolor sit amet'
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <Clock />
-        <Person />
+        <Sidebar post={post} />
+        <Post post={post} totalComments={12}/>
       </header>
     </div>
   );
