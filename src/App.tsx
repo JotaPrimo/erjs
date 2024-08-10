4673486357259d5d5b21eae532a9a2a1aa0ff775
jota santos <gestald118@email.com>
2024-08-10 09:29:02 -0400

    2.16. Encapsulando componentes com props.children

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Post from './components/Post';
-import Sidebar from './components/Sidebar';
+import Card from './components/Card';
 
 
 // trabalhando com props
@@ -12,7 +12,7 @@ function App() {
   // toda vez que uma prop tem seu state alterado, o componente re-redenderiza
   // por isso que quando se usar o set parta alterar o state post,  que é uma prop de post
   // ele re-renderiza
-  const [post, setPost] = useState({
+  const [post] = useState({
     title: 'Título muito maneiro',
     content: 'Lorem ipsum dolor sit amet'
   })
@@ -20,8 +20,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Sidebar post={post} />
-        <Post post={post} totalComments={12}/>
+        <Card title='Card do texto'>
+          <Post post={post} totalComments={12} />
+        </Card>
       </header>
     </div>
   );
---
+++ src/components/Card.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from "react";
+
+type CardProps = PropsWithChildren<{
+    title: string
+}>
+
+export default function Card(props: CardProps) {
+    console.log(props.children);
+    
+    return <div
+        style={{
+            background: 'rgba(0,0,0,.25)',
+            borderRadius: 8,
+            padding: 16
+        }}>
+            <div>{props.title}</div>
+        { props.children }
+    </div>
+}
