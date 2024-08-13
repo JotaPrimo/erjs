2b367deef7255a688ee63d007b2e145f0479e39a
jota santos <gestald118@email.com>
2024-08-13 08:13:18 -0400

    2.21. Criando um Class Component (Componente baseado em classe)

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { Component, ReactNode } from "react";
+
+// criando class components, forma antiga de estilizar
+// componente baseado em classe
+class Button  extends Component {
+    render(): ReactNode {
+        return <div>
+            todo: button
+        </div>
+    }
+}
+
+
+export default Button
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from "react";
 import * as C from './Cars.styles';
+import Button from "./Button";
 
 type CardProps = PropsWithChildren<{
     title: string,
@@ -15,6 +16,11 @@ export default function Card(props: CardProps) {
                 {props.title}
             </C.Title>
             {props.children}
+            <div>
+                <Button>
+                    Ver mais
+                </Button>
+            </div>
         </C.Wrapper>
     )
 }
