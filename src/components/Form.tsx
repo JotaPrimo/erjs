22853683b9f13340a032b7075def3c0d104ed5c0
jota santos <gestald118@email.com>
2024-08-14 09:43:35 -0400

    form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,15 +2,43 @@ import { useState } from "react"
 
 export default function Form () {
 
+    const[form, setForm] = useState({
+        name: '',
+        sobrenome: '',
+        email: '',
+        senha: ''
+    })
+
     const [name, setName] = useState('Jota')
 
     return <div>
         <label htmlFor="Nome">
             <input 
                 type="text" 
-                value={name}
+                value={form.name}
                 onChange={e => setName(e.target.value)}
                 placeholder="Nome" />
+        </label>
+        <label htmlFor="Sobrenome">
+            <input 
+                type="text" 
+                value={form.sobrenome}
+                onChange={e => setName(e.target.value)}
+                placeholder="Sobrenome" />
+        </label>
+        <label htmlFor="Email">
+            <input 
+                type="text" 
+                value={form.email}
+                onChange={e => setName(e.target.value)}
+                placeholder="Email" />
+        </label>
+        <label htmlFor="Senha">
+            <input 
+                type="text" 
+                value={form.senha}
+                onChange={e => setName(e.target.value)}
+                placeholder="Senha" />
         </label>
         <div>
             { name }
