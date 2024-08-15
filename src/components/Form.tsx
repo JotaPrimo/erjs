ada6a1d76228c30b8b95251fee98ef4021fec833
jota santos <gestald118@email.com>
2024-08-15 07:52:06 -0400

    2.30. Dica preciosa para lidar com formulários

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,47 +1,65 @@
 import { useState } from "react"
 
-export default function Form () {
+export default function Form() {
 
-    const[form, setForm] = useState({
+    const [form, setForm] = useState({
         name: '',
         sobrenome: '',
         email: '',
         senha: ''
     })
 
-    const [name, setName] = useState('Jota')
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log(form);
+    }
 
-    return <div>
+    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
+    return <form onSubmit={handleSubmit}>
         <label htmlFor="Nome">
-            <input 
-                type="text" 
+            <input
+                type="text"
+                name="name"
                 value={form.name}
-                onChange={e => setName(e.target.value)}
+                onChange={handleInputChange}
                 placeholder="Nome" />
         </label>
+        <br />
         <label htmlFor="Sobrenome">
-            <input 
-                type="text" 
+            <input
+                type="text"
+                name="sobrenome"
                 value={form.sobrenome}
-                onChange={e => setName(e.target.value)}
+                onChange={handleInputChange}
                 placeholder="Sobrenome" />
         </label>
+        <br />
         <label htmlFor="Email">
-            <input 
-                type="text" 
+            <input
+                type="text"
+                name="email"
                 value={form.email}
-                onChange={e => setName(e.target.value)}
+                onChange={handleInputChange}
                 placeholder="Email" />
         </label>
+        <br />
         <label htmlFor="Senha">
-            <input 
-                type="text" 
+            <input
+                type="password"
+                 name="senha"
                 value={form.senha}
-                onChange={e => setName(e.target.value)}
+                onChange={handleInputChange}
                 placeholder="Senha" />
         </label>
         <div>
-            { name }
+            <button type="submit">Submit</button>
         </div>
-    </div>
+    </form>
 }
