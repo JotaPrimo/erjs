e0c3efecc177ac5f6c8ea49cb581d92984e52779
jota santos <gestald118@email.com>
2024-08-19 08:57:41 -0400

    3.3. Criando um hook personalizado

---
+++ 2-hooks/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import useCounter from './hooks/useCounter';
+
+function App() {
+
+  const number = useCounter()
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        { number }
+      </header>
+    </div>
+  );
+}
+
+export default App;
