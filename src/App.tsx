ed6b7ce1e85f0bb6b3ae3ee95631151ece1a7e6b
jota santos <gestald118@email.com>
2024-08-19 08:41:24 -0400

    finalizado, iniciando novo projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const names = ['Daniel', 'Jota', 'João', 'Emanuel']
+  
+  const [names, setNames] = useState(
+    [
+      'Daniel', 'Jota', 'João', 'Emanuel',
+      'Jessica', 'Caleb', 'Jughead'
+    ]
+  )
+
+
+  // gerenciar efeitos colaterais... buscando dados na api
+  useEffect( () => {
+    // será exeutado no momento do didmount event
+    console.log(names);    
+  }, [names])
+  // ao inserir como depencia o names, toda vez que  for altearado seu estado
+  // esse effect será executado novamente
+  
   return (
     <div className="App">
       <header className="App-header">     
@@ -10,6 +27,12 @@ function App() {
             names.map((name, index) => <li key={index}>{name}</li>)
           }
         </ul>
+
+        <button onClick={() => {
+          setNames([...names, 'Jeremias'])
+        }}>
+          Atualizar lista
+        </button>
       </header>
     </div>
   );
