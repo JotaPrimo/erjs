07ea2e0c5cb8db25b9dcc18725f09646ae277ca0
jota santos <gestald118@email.com>
2024-08-29 08:52:16 -0400

    useRef

--- 2-hooks/src/App.tsx
+++ 2-hooks/src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import './App.css';
 import PeopleList from './hooks/PeopleList';
 import useCounter from './hooks/useCounter';
@@ -5,11 +6,18 @@ import useCounter from './hooks/useCounter';
 function App() {
 
   const number = useCounter(0)
+  const counter = useRef(2) // este valor é mutavel, pode receber um valor initial
+  // counter.current = 5 essa alteração é valida, e não causa re-renderização
+  // do componente
 
   return (
     <div className="App">
       <header className="App-header">
         { number }
+
+
+      <div style={{ backgroundColor: 'blueviolet'}}>{counter.current}</div>
+
        <PeopleList />
        <PeopleList />
       </header>
