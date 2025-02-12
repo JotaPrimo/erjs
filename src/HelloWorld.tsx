364318a99add0b6a2c41d424c464c4d4785a1a54
Jota Santos <jailsonsantosdpge@outlook.com>
2025-02-12 13:19:06 -0400

    2.10. Atributos com JSX

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -8,7 +8,7 @@ export function HelloWorld() {
     return (
         <div>
             <h1>Hello World React</h1>
-            <p>{ name ? name : 'Danielo' }</p>
+            <p tabIndex={0} style={{ background: 'red'}}>{ name ? name : 'Danielo' }</p>
             <p>Isto é um componente react</p>
             <p>Sem escolha, vamos entender esse canalha</p>
         </div>
