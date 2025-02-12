3ccac11644a3e02ece14a0042af87328ab548f32
Jota Santos <jailsonsantosdpge@outlook.com>
2025-02-12 12:29:05 -0400

    2.6. Criando o componente de Hello World

---
+++ src/HelloWorld.tsx
@@ -0,0 +1,10 @@
+/** um arquivo que exporta uma função que retorna html */
+export function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Isto é um componente react</p>
+            <p>Sem escolha, vamos entender esse canalha</p>
+        </div>
+    )
+}
