bb4f16523893db3096f309af1e02540c27551bf7
Jota Santos <jailsonsantosdpge@outlook.com>
2025-02-13 18:54:13 -0400

    2.23. Estados em Class Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import { useState } from 'react';
 import './App.css';
-import Post from './components/Post';
 import Card from './components/Card';
-
+import Post from './components/Post';
 
 function App() {
-
   const [post] = useState({
-    title: 'Jogão de bola',
-    content: 'It is a long established fact that a reader will be distracted'
-  });
+    title: 'Título maneiro',
+    content: 'Lorem Ipsum dolor sit amet'
+  })
 
   return (
-    <div className="App">      
-      <header className="App-header">       
-        <Card align='left' title='Titulo do Card'>
-          <Post post={post} />
+    <div className="App">
+      <header className="App-header">
+        <Card align="left" title={"Card"}>
+          <Post post={post} totalComments={12} />
         </Card>
-        </header>
+      </header>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,34 @@
-import { Component } from "react";
+import { Component, ReactNode } from "react";
 
 interface ButtonProps {
-  onClick?: () => any
+  onClick?: () => any,
+  children?: ReactNode
+  initializeClicked?: boolean
 }
 
-class Button extends Component<ButtonProps> {
+interface ButtonState {
+  alreadyClicked: boolean
+}
+
+class Button extends Component<ButtonProps, ButtonState> {
+  constructor (props: ButtonProps) {
+    super(props)
+
+    this.state = {
+      alreadyClicked: !!props.initializeClicked
+    }
+  }
+
   render () {
-    return <button onClick={this.props.onClick}>
+    return <button
+      onClick={() => {
+        this.setState({
+          alreadyClicked: true
+        })
+        this.props.onClick?.call([])
+      }}
+      disabled={this.state.alreadyClicked}
+    >
       { this.props.children }
     </button>
   }
