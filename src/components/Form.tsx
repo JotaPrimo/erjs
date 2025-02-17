e955d1723990423ba6fa5c7de52e9d3ccb753460
Jota Santos <jailsonsantosdpge@outlook.com>
2025-02-17 16:35:21 -0400

    2.30. Dica preciosa para lidar com formulários

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,69 @@
 import { useState } from "react";
 
 function Form() {
+  const [form, setForm] = useState({
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
 
-    const[name, setName] = useState('Jota')
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(form);
+  }
+  
+  function handleInputChange (e: React.ChangeEvent<HTMLInputElement>) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
 
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={form.name}
+        name="name"
+        placeholder="Nome"
+        onChange={handleInputChange}
+      />
 
-    return ( 
-        <div>
-          <input 
-            type="text" 
-            value={name}
-            placeholder="Nome"
-            onChange={ e => setName(e.target.value) }
-          />
+      <br />
 
-          <div>
-            { name }
-          </div>
-        </div>
-     );
+      <input
+        type="text"
+        name="lastname"
+        value={form.lastname}
+        placeholder="Sobrenome"
+        onChange={handleInputChange}
+      />
+
+      <br />
+
+      <input
+        type="email"
+        name="email"
+        value={form.email}
+        placeholder="Email"
+        onChange={handleInputChange}
+      />
+
+      <br />
+
+      <input
+        type="password"
+        name="password"
+        value={form.password}
+        placeholder="Password"
+        onChange={handleInputChange}
+      />
+
+      <br />
+      <button type="submit">Salvar</button>
+    </form>
+  );
 }
 
 export default Form;
